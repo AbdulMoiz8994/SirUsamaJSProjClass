ce147b85428372054e6e264f18ae474c973bae51
AbdulMoiz8994 <moiza8994@gmail.com>
2021-11-01 15:48:22 +0500

    Add the Project1Quiz

--- Project1Quiz/script.js
+++ Project1Quiz/script.js
@@ -13,11 +13,42 @@ let score=0
 
    if(option1.checked == true ){
      score++
-     alert('You Answer is correct')
+     alert('Your Question 1  is correct')
    }else{
-    alert('You Answer is wrong')
+    alert('Your Question 1 is wrong')
    }
-   alert(score)
 
+//    condition 2 check
+//    const quest2_option1=document.getElementById('q2_a1')
+//    const quest2_option2=document.getElementById('q2_a2')
+   const quest2_option3=document.getElementById('q2_a3')
+ if(quest2_option3.checked == true){
+     score++
+     alert('Your Question 2 is correct')
+ }else{
+    alert('Your Question 2 is wrong')
+   }
+
+//    alert(score)
+//    const quest3_option1=document.getElementById('q3_a1')
+//    const quest3_option2=document.getElementById('q3_a2')
+   const quest3_option3=document.getElementById('q3_a3')
+   if(quest3_option3.checked == true){
+      score++ 
+      alert('Your Question 3 is correct') 
+   }else{
+    alert('Your Question 3 is wrong')
+   }
+
+   const quest4_option1=document.getElementById('q4_a1')
+//    const quest4_option2=document.getElementById('q4_a2')
+//    const quest4_option3=document.getElementById('q4_a3')
+   if(quest4_option1.checked == true){
+    score++ 
+    alert('Your Question 4 is correct') 
+ }else{
+  alert('Your Question 4 is wrong')
+ }
+alert(score)
 
 }
